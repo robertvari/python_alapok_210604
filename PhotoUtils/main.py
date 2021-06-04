f728699e4801bbd664128279999048b900b8e718
Robert Vári <mail.robertvari@gmail.com>
2021-06-04 15:36:57 +0200

    init

--- PhotoUtils/main.py
+++ PhotoUtils/main.py
@@ -3,6 +3,8 @@ from utilities.Photo import Photo
 
 def main():
     photo = Photo()
+    photo.open(r"C:\Work\_PythonSuli\python_alapozo\photos\IMG_1069.JPG")
+    photo.report()
 
 
 if __name__ == '__main__':
--- PhotoUtils/utilities/Photo.py
+++ PhotoUtils/utilities/Photo.py
@@ -1,2 +1,49 @@
+import os
+from PIL import Image, ExifTags
+
+
 class Photo:
-    pass
+    def __init__(self):
+        self._file_name = None
+        self._size = None
+        self._date = None
+        self._img = None
+
+    def open(self, file_path):
+        assert os.path.exists(file_path), f"File does not exist: {file_path}"
+        assert file_path.lower().endswith(".jpg"), "File must be .jpg"
+
+        self._file_name = os.path.basename(file_path)
+
+        self._img = Image.open(file_path)
+        self._size = self._img.size
+
+        # collect data from image meta
+        self._get_exif_data()
+
+    def _get_exif_data(self):
+        if not self._img:
+            return
+
+        exif_data = self._img._getexif()
+
+        if not exif_data:
+            return
+
+        for key, value in exif_data.items():
+            tag_name = ExifTags.TAGS.get(key)
+
+            if tag_name == "DateTime":
+                self._date = value
+
+    def report(self):
+        print("-"*50)
+
+        print(f"File: {self._file_name}")
+        print(f"Size: {self._size}")
+        print(f"Date: {self._date}")
+
+        print("-" * 50)
+
+    def __str__(self):
+        return self._file_name
