f33fa994ed7805c5b6f51a6f283c3947f91e4b1f
Robert Vári <mail.robertvari@gmail.com>
2021-06-04 16:00:41 +0200

    update

--- PhotoUtils/main.py
+++ PhotoUtils/main.py
@@ -3,8 +3,9 @@ from utilities.Photo import Photo
 
 def main():
     photo = Photo()
-    photo.open(r"C:\Work\_PythonSuli\python_alapozo\photos\IMG_1069.JPG")
-    photo.report()
+    photo.open(r"C:\Work\_PythonSuli\python_alapozo\photos\cedric-letsch-ie2xdSo3POc-unsplash.jpg")
+    photo.create_report()
+    photo.watermark()
 
 
 if __name__ == '__main__':
--- PhotoUtils/utilities/Photo.py
+++ PhotoUtils/utilities/Photo.py
@@ -5,21 +5,48 @@ from PIL import Image, ExifTags
 class Photo:
     def __init__(self):
         self._file_name = None
+        self._dir_name = None
         self._size = None
         self._date = None
         self._img = None
+
+    @property
+    def path(self):
+        return os.path.join(self._dir_name, self._file_name)
+
+    @property
+    def date(self):
+        if self._date:
+            return self._date
+
+        return "Date not found!"
 
     def open(self, file_path):
         assert os.path.exists(file_path), f"File does not exist: {file_path}"
         assert file_path.lower().endswith(".jpg"), "File must be .jpg"
 
         self._file_name = os.path.basename(file_path)
+        self._dir_name = os.path.dirname(file_path)
 
         self._img = Image.open(file_path)
         self._size = self._img.size
 
         # collect data from image meta
         self._get_exif_data()
+
+    def delete(self):
+        user_input = input(f"Do you really want to delete: {self._file_name}(y/n)")
+        if user_input == "y":
+            self._img.close()
+
+            os.remove(self.path)
+            print(f"File {self._file_name} was deleted")
+
+            self._file_name = None
+            self._dir_name = None
+            self._size = None
+            self._date = None
+            self._img = None
 
     def _get_exif_data(self):
         if not self._img:
@@ -47,3 +74,17 @@ class Photo:
 
     def __str__(self):
         return self._file_name
+
+    def create_report(self):
+        """
+        Saves an xlsx file into the photo folder
+        :return:
+        """
+        pass
+
+    def watermark(self):
+        """
+        Creates a smaller, watermarked version of the photo.
+        :return:
+        """
+        pass
