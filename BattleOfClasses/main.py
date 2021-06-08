2ff4c9097d07bfcc3115fbdb979859dd848a24e0
Robert Vári <mail.robertvari@gmail.com>
2021-06-08 14:53:18 +0200

    init

---
+++ BattleOfClasses/main.py
@@ -0,0 +1,11 @@
+class BattleOfClasses:
+    def __init__(self):
+        self._intro()
+
+    @staticmethod
+    def _intro():
+        print("-"*50, "Battle of Classes", "-"*50)
+
+
+if __name__ == '__main__':
+    BattleOfClasses()
