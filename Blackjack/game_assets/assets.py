0a8de84c197003dea7b4a1f45a8d9638f30c636d
Robert Vári <mail.robertvari@gmail.com>
2021-06-08 12:38:01 +0200

    update

--- Blackjack/game_assets/assets.py
+++ Blackjack/game_assets/assets.py
@@ -10,6 +10,33 @@ class AIPlayer(PlayerBase):
     pass
 
 
+class Deck:
+    def __init__(self):
+        self.cards = []
+        self.create()
+
+    def create(self):
+        self.cards.clear()
+
+        cards = [
+            ["2", 2],
+            ["3", 3],
+            ["4", 4],
+            ["5", 5],
+            ["6", 6],
+            ["7", 7],
+            ["8", 8],
+            ["9", 9],
+            ["10", 10],
+            ["King", 10],
+            ["Queen", 10],
+            ["Jack", 10],
+            ["Ace", 11]
+        ]
+
+        names = ["Heart", "Club", "Diamond", "Spade"]
+
+
 class Card:
     def __init__(self, name, value):
         self._name = name
